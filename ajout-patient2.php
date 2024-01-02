184769de881742446fc2bda7d2d1a2dfd949d695
StephGarage404 <stephane.1995@hotmail.fr>
2024-01-02 16:55:41 +0100

    stephane pdo

---
+++ ajout-patient2.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "connexion.php";
+
+if (!empty($_POST['firstname'])
+&& !empty($_POST['name'])
+&& !empty($_POST['birthday'])
+&& !empty($_POST['email'])
+&& !empty($_POST['telephone'])) 
+{
+    // Enregistrer en BDD
+    
+    //Etape 2 Créer la requete SQL
+    
+    $sql = "INSERT INTO `patients`(`firstname`, `lastname`, `mail`, `phone`, `birthdate`) VALUES ('"
+            . $_POST['firstname']. "', '" 
+            . $_POST['name'] . "',' "
+            . $_POST['email'] . "', '"
+            . $_POST['telephone'] . "', '"
+            . $_POST['birthday'] ."')";
+    var_dump($sql);
+    $connexion->query($sql);  
+}
+
+else
+{
+   echo "requête invalide";
+   echo "<pre>";
+   var_dump($sql);
+   echo "</pre>"; 
+}
---
+++ connexion.php
@@ -0,0 +1,5 @@
+<?php
+$connexion = new PDO(
+    'mysql:host=127.0.0.1;dbname=hospitalE2N;port:3306', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,]
+);
+?>
